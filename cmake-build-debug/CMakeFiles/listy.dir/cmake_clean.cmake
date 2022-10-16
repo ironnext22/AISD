6edf8f896b715736d08c6523493ac3eade129bdb
ironnext22 <97704600+ironnext22@users.noreply.github.com>
2022-10-17 01:01:24 +0200

    1

---
+++ cmake-build-debug/CMakeFiles/listy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listy.dir/main.cpp.obj"
+  "CMakeFiles/listy.dir/main.cpp.obj.d"
+  "liblisty.dll.a"
+  "listy.exe"
+  "listy.exe.manifest"
+  "listy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
